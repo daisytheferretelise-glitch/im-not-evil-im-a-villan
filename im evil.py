87db6eb458819da3d4647ba792d3147ffeefeea7
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-19 20:08:32 +0000

    Were writing iformation about varbieals and classes

---
+++ im evil.py
@@ -0,0 +1,9 @@
+NO_im_a_alpha=5 
+print(type(NO_im_a_alpha))
+
+NO_im_a_alpha="Now I'm a string"
+print(type(NO_im_a_alpha))
+c=0.67
+print(type(c))
+D=False
+print(type(D))
---
+++ type casting.py
@@ -0,0 +1,18 @@
+Name= "Suzy"
+Age= 15
+Is_student= True
+Weight= 33.5
+Height= 5.7
+print(type(Name))
+print(type(Age))
+print(type(Is_student))
+print(type(Weight))
+print(type(Height))
+
+Weight= int(Weight)
+print(type(Weight))
+
+Height= int(Height)
+print(type(Height))
+print(Weight)
+print(Height)
