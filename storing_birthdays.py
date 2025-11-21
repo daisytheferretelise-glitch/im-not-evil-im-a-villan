1984a0bc102dc89fc048a4766e6c1e0c06d3dc11
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-21 18:49:08 +0000

    BOB IS NOT FAKE

---
+++ storing_birthdays.py
@@ -0,0 +1,6 @@
+print("Random person1")
+Random_person_1= "febuary 29 3000"
+print(Random_person_1)
+print("Definetly not fake person Bob")
+Definetly_not_fake_person_Bob="BURBY  67 6700"
+print(Definetly_not_fake_person_Bob)
