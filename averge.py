d6e40e6d26fdc43147a207e1f0f500e576f9a41a
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-21 18:36:52 +0000

    its about my trees and how many C:

---
+++ averge.py
@@ -0,0 +1,12 @@
+print("apple tree 1 ")
+print("apple tree 2 ")
+print("apple tree 3 ")
+print("apple tree 4 ")
+Apple_tree_1=1000
+Apple_tree_2=1500
+Apple_tree_3=2000
+Apple_tree_4=2500
+sum=Apple_tree_1+Apple_tree_2+Apple_tree_3+Apple_tree_4
+average=sum/4
+print("The avarage number of apples on my apple trees is")
+print(average)
