84099f1dae2783d03c762ce2bcc1f76519f47591
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-26 19:25:16 +0000

    bob is rich

---
+++ negatie or postive and bob is a sigma.py
@@ -0,0 +1,13 @@
+print("bob has 3 haribos")
+bob=3
+if bob>0:
+    print("bob is rich and has haribos and it's positive")
+else:
+    print("bob is broke and has no haribos furthermore bob is in the neagitive")
+
+    print("Mrs bob has -2 haribos")
+Mrs_bob=-2
+if Mrs_bob>0:
+    print("Mrs bob is rich and has haribos and it's positive")
+else:
+    print("Mrs bob is broke and has no haribos furthermore Mrs bob is in the neagitive")
