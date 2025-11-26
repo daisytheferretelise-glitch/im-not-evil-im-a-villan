3db03a8c0962dbd13726eab30d34739513228d59
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-26 19:39:19 +0000

    bob is to rich

---
+++ bob is broke time to make a profit.py
@@ -0,0 +1,7 @@
+print("Bob has bought 10 items for £70")
+bobs_buying_price=70
+bobs_selling_price=100
+if bobs_buying_price<bobs_selling_price:
+    print("Bob has made a profit of £", bobs_selling_price - bobs_buying_price)
+else:
+    print("Bob has made a loss of £", bobs_buying_price - bobs_selling_price)
