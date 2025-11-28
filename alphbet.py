ee9827320a055453c0a739e6507a88e5a78156fe
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-28 19:45:44 +0000

    alphabet

---
+++ alphbet.py
@@ -0,0 +1,4 @@
+
+input("Enter a letter only one  ot not a letter but still only one")
+if a or b or c or  d or e or f or g or h or i or j or k or l or m or n and o or p or q or r or s or t or u or v or w or x or y or z = True:
+    print(" This is part of the alphabet")
