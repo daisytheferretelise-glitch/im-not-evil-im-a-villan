3db4e5592a2c0665655457266ce83d064a518231
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-28 18:30:06 +0000

    bob wont be cold

---
+++ clothes.py
@@ -0,0 +1,8 @@
+print("Bob is checkig the weather but he broke alexa so he ask you to do it")
+Cold=10
+Warm=20
+if Cold>Warm:
+
+print("It's cold outside, Bob should wear a coat")
+else:
+print("It's warm outside, Bob can wear a t-shirt")
