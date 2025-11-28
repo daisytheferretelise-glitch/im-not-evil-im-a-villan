2ed1932b2936738382e60b4e08c2553949b9a068
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-11-28 19:11:53 +0000

    how to use ogical operaters

---
+++ Bob is tiered sing him a lullaby using your logical operaters.py
@@ -0,0 +1,15 @@
+a="MOTHER"
+b=0
+c="full speed ahead"
+if a and b and c:
+    print("Then mother is going full speed ahead in 0 cecius")
+else:
+    print("Mother is not going full speed ahead in 0 cecius but probaly in 15")
+
+d=10
+e=0
+f=10
+if d>0 or e>0 or f>0:
+    print("no count again")
+else:
+    print("Perfectly right")
--- clothes.py
+++ clothes.py
@@ -4,5 +4,6 @@ Warm=20
 if Cold>Warm:
 
 print("It's cold outside, Bob should wear a coat")
+
 else:
 print("It's warm outside, Bob can wear a t-shirt")
