1e344fb1f50fd26e466d64c03a8cdc3382af82cb
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-03 20:00:05 +0000

    identity operators

---
+++ john is better than bob.py
@@ -0,0 +1,14 @@
+print("John the rizzer has a dougnuts")
+print("ugly bob with 0 rizz thinks he has more dougnuts than john and rizz")
+jd=10
+jr=10000000
+bd=6
+br=0
+if (type(jr) is int):
+    print("true John is a sigma rizzler")
+else:
+    print("false bob is a beta chad but stil better than john lol")
+if (type(br)is not int):
+        print("omg bob is a beta chad")
+else:
+     print("JOHN IS RIZZEYER THAN BOB")
