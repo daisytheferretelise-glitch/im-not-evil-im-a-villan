adf15b62931725aa09226c3b246a7e58ce6399b8
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-03 19:32:00 +0000

    dogs

---
+++ Dog.py
@@ -0,0 +1,15 @@
+print("John has a dog and wants to join a competion to show how many tricks his dog can do")
+print("he adopts a 5 more dogs")
+
+Dog_tricks:6#bro can only do 6 tricks lol
+Dog_2_tricks:11#bro can only do 11 tricks strange
+Dog_3_tricks:4#im not sure why i made this note
+Dog_4_tricks:17#im not sure why i made this note
+Dog_5_tricks:67#im not sure why i made this note
+Dog_6_tricks:21#im not sure why i made this note
+
+Compettition_dog_tricks:100
+if Dog_tricks+Dog_2_tricks+Dog_3_tricks+Dog_4_tricks+Dog_5_tricks+Dog_6_tricks>=Compettition_dog_tricks:
+    print("John has won the competion")#
+else:
+    print("John buried his dogs in the back yard and also lost the competion")
