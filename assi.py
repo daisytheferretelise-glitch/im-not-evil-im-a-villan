1271579507f471e34698f134a4e72561d1aafd31
daisytheferretelise-glitch <daisytheferretelise@gmail.com>
2025-12-05 18:11:34 +0000

    ACSCII

---
+++ assi.py
@@ -0,0 +1,9 @@
+# Program to find ASCII value of a character
+
+# Take input from user
+char = input("Enter a character: ")
+
+# ord() function returns the ASCII value
+ascii_value = ord(char)
+
+print(f"The ASCII value of '{char}' is {ascii_value}")
